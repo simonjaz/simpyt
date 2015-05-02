c00468b07d5e79bb21c1b923973eca18a696453c
Simon Jazbar <simon@celtra.com>
2015-05-02 21:22:55 +0200

    test

--- assigment_2_3.py
+++ assigment_2_3.py
@@ -2,6 +2,8 @@
 # Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). 
 # You should use raw_input to read a string and float() to convert the string to a number. 
 # Do not worry about error checking or bad user data.
+#
+#
 
 hrs = raw_input("Enter Hours: ")
 rate = raw_input("Enter Rate:\n")
